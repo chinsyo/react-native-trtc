95938a249ae6b40355c7eef14e8305f8ed9f2c2e
Chinsyo <chinsyo@sina.cn>
2020-04-05 12:04:36 +0800

    bump to 0.3.0

---
+++ ios/RNTXCloudVideoView.h
@@ -0,0 +1,20 @@
+//
+//  RNTXCloudVideoView.h
+//  RNTrtc
+//
+//  Created by 余保荣 on 2020/2/24.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <React/RCTViewManager.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNTXCloudVideoView : UIView
+
+@property (nonatomic, copy) NSString *userId;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/RNTXCloudVideoViewManager.h
@@ -0,0 +1,17 @@
+//
+//  RNTXCloudVideoViewManager.h
+//  RNTrtc
+//
+//  Created by 余保荣 on 2020/2/24.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import <React/RCTViewManager.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNTXCloudVideoViewManager : RCTViewManager
+
+@end
+
+NS_ASSUME_NONNULL_END
